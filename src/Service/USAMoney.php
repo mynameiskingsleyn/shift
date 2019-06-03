ce066aa24bb3baa819f5385baf310297b5ece686
Kingsley N <mynameiskingsleyn@gmail.com>
2019-06-03 02:52:03 -0700

    modified USAMoney to handle empty table

--- src/Service/USAMoney.php
+++ src/Service/USAMoney.php
@@ -24,18 +24,22 @@ class USAMoney extends WorldMoney implements AppMoneyInterface
         $this->moneyRepo = $moneyRepo;
         $this->denomRepo = $denomRepo;
         $money = $this->moneyRepo->findBy(['name'=>'UsaMoney']);
-        $this->ourMoney = $money[0];
-        $denoms = $this->ourMoney->getDenoms();
-        if ($denoms) {
-            foreach ($denoms->toArray() as $adenom) {
-                $this->denom[$adenom->getName()]= $adenom->getValue();
-                $this->bank[$adenom->getName()] = 0;
+        $this->denom =['Hundred'=>100,'Fifty'=>50,'Twenty'=>20,'Ten'=>10,'Five'=>5,'One'=>1,'Quarter'=>.25,
+                    'Dime'=>.10,'Nickle'=>.05,'Penny'=>.01];
+        $this->bank = ['Hundred'=>0,'Fifty'=>0,'Twenty'=>0,'Ten'=>0,'Five'=>0,'One'=>0,'Quarter'=>0,'Dime'=>0,'Nickle'=>0,'Penny'=>0];
+
+        if ($money) {
+            $this->ourMoney = $money[0];
+            $denoms = $this->ourMoney->getDenoms();
+            if ($denoms) {
+                foreach ($denoms->toArray() as $adenom) {
+                    $this->denom[$adenom->getName()]= $adenom->getValue();
+                    $this->bank[$adenom->getName()] = 0;
+                }
             }
-        } else {
-            $this->denom =['Hundred'=>100,'Fifty'=>50,'Twenty'=>20,'Ten'=>10,'Five'=>5,'One'=>1,'Quarter'=>.25,
-                        'Dime'=>.10,'Nickle'=>.05,'Penny'=>.01];
-            $this->bank = ['Hundred'=>0,'Fifty'=>0,'Twenty'=>0,'Ten'=>0,'Five'=>0,'One'=>0,'Quarter'=>0,'Dime'=>0,'Nickle'=>0,'Penny'=>0];
         }
+
+
         //=$this->moneyRepo->findBy(['name'=>'UsaMoney'], ['created_at'=>'DESC']);
     }
     public function setTransaction($totalCost=0, $amountProvided=0)
